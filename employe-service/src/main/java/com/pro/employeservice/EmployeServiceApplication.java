6357b80ca420ef42cfbecf6c745098fd01c5000d
varmagurrala <varmaagurrala@gmail.com>
2023-04-21 18:40:56 +0530

    upto rest templet

---
+++ employe-service/src/main/java/com/pro/employeservice/EmployeServiceApplication.java
@@ -0,0 +1,17 @@
+package com.pro.employeservice;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class EmployeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeServiceApplication.class, args);
+	}
+	
+	
+
+}
---
+++ employe-service/src/main/java/com/pro/employeservice/controller/EmployeeController.java
@@ -0,0 +1,40 @@
+package com.pro.employeservice.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pro.employeservice.dto.EmployeeDto;
+import com.pro.employeservice.service.EmployeeService;
+
+@RestController
+@RequestMapping
+public class EmployeeController {
+
+	@Autowired
+	private EmployeeService employeeService;
+	
+	@PostMapping("/create")
+	public ResponseEntity<EmployeeDto> createEmployee(@RequestBody EmployeeDto empolyee){
+		
+		return new  ResponseEntity<>(employeeService.saveEmployee(empolyee),HttpStatus.CREATED);
+	}
+	
+	@GetMapping("/get/{empid}")
+	public ResponseEntity<EmployeeDto> getById(@PathVariable(name="empid") Long empid ){
+		
+		EmployeeDto emp= employeeService.getById(empid);
+		
+		return new  ResponseEntity<EmployeeDto>(emp,HttpStatus.OK);
+		
+	}
+	
+	
+}
